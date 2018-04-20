3e163a648a84e61f4415c084d0035133c7106ece
arcticdreamz <tomcrasset@gmail.com>
2018-04-20 21:16:10 +0200

    y

--- MastermindServer.java
+++ MastermindServer.java
@@ -11,46 +11,6 @@ public class MastermindServer {
 		
 		try {
 
-			URL url = new URL("http://localhost/Mastermind/test.php");
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			connection.setRequestMethod("GET");
-
-			/******Usefull methods
-			String method = connection.getRequestMethod();
-			int status = connection.getResponseCode();
-
-			System.out.println("Status :" + status);
-			System.out.println("Method : " + method);
-			*/
-
-			String headerField;
-			int i = 0;
-			while((headerField = connection.getHeaderField(i)) != null){
-				System.out.println(headerField);
-				i++;
-			}
-
-			/****** Getting the whole HTTP page (not useful)
-
-
-			BufferedReader testBR = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			StringBuffer buf = new StringBuffer();
-			String line;
-
-			while((line = testBR.readLine()) != null){
-				buf.append(line);
-			}
-			
-			testBR.close();
-			connection.disconnect();
-
-			System.out.println(buf.toString());
-			*/
-
-
-
-
-			/*
 			ss = new ServerSocket(2140);
 			System.out.println("Server started...");
 			
@@ -61,19 +21,19 @@ public class MastermindServer {
 				t.start();
 			}
 
-			*/
+			
 		}
 		catch(IOException e) {
 			e.printStackTrace();
 		}
-		/*finally {
+		finally {
 			try {
 				ss.close();
 			}
 			catch(IOException e1) {
 				e1.printStackTrace();
 			}
-		}*/
+		}
 	}
 }
 
