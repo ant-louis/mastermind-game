96e12d95f18f73ee6b17035b1a8d82d797304727
arcticdreamz <tomcrasset@gmail.com>
2018-04-20 21:05:52 +0200

    Added small test.php file; Getting the header of the HTTPRequest

--- MastermindServer.java
+++ MastermindServer.java
@@ -5,11 +5,52 @@ import java.net.*;
 public class MastermindServer {
 	
 	private static ServerSocket ss;
-	
+
 	
 	public static void main ( String argv[] ) {
 		
 		try {
+
+			URL url = new URL("http://localhost/Mastermind/test.php");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+
+			/******Usefull methods
+			String method = connection.getRequestMethod();
+			int status = connection.getResponseCode();
+
+			System.out.println("Status :" + status);
+			System.out.println("Method : " + method);
+			*/
+
+			String headerField;
+			int i = 0;
+			while((headerField = connection.getHeaderField(i)) != null){
+				System.out.println(headerField);
+				i++;
+			}
+
+			/****** Getting the whole HTTP page (not useful)
+
+
+			BufferedReader testBR = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			StringBuffer buf = new StringBuffer();
+			String line;
+
+			while((line = testBR.readLine()) != null){
+				buf.append(line);
+			}
+			
+			testBR.close();
+			connection.disconnect();
+
+			System.out.println(buf.toString());
+			*/
+
+
+
+
+			/*
 			ss = new ServerSocket(2140);
 			System.out.println("Server started...");
 			
@@ -19,18 +60,20 @@ public class MastermindServer {
 				Thread t = new Thread(new MastermindServerWorker(client));
 				t.start();
 			}
+
+			*/
 		}
 		catch(IOException e) {
 			e.printStackTrace();
 		}
-		finally {
+		/*finally {
 			try {
 				ss.close();
 			}
 			catch(IOException e1) {
 				e1.printStackTrace();
 			}
-		}
+		}*/
 	}
 }
 
