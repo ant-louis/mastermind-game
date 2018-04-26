30de9c49912dda457688f914aebe59fe43b33d1e
arcticdreamz <tomcrasset@gmail.com>
2018-04-26 20:54:12 +0200

    Started a Server to game interface- Basic sketch only

--- MastermindClient.java
+++ MastermindClient.java
@@ -1,33 +1,15 @@
-import java.io.*;
-import java.net.*; 
+import java.net.*;
 
-
-public class MastermindClient {
-	
-	//Variables
-	private static Socket socket;
-	
-	
-	//Main
-	public static void main(String[] args) {
+class MastermindClient {
 		
-		try {
-			socket = new Socket("localhost", 2140);
-			MastermindClientManager client = new MastermindClientManager(socket);
-			client.play();
+	public static void main (String args[]){
+			
+		try{
+			Socket clientsock = new Socket("localhost",2416);
+			new Game(clientsock);
 		}
-		catch(SocketTimeoutException e){
-			System.err.println("Error the server doesn't response.Cause : "+e.getMessage());
-		}
-		catch(UnknownHostException e) {
-			System.err.println("Can not connect the address " + socket.getLocalAddress());
-		}
-		catch(SocketException e){
-			System.err.println("Error the server doesn't response.Cause : "+e.getMessage());
-		}
-		catch(IOException e) {
+		catch(Exception e){
 			e.printStackTrace();
 		}
-		
 	}
 }
